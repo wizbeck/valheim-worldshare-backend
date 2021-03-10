ac1a46a8ab99d4eab3dc97c92759ea84b3806709
Wesley <wes.beck21@outlook.com>
2021-03-09 17:29:46 -0800

    create route and controller action

--- app/controllers/api/v1/worlds_controller.rb
+++ app/controllers/api/v1/worlds_controller.rb
@@ -1,2 +1,25 @@
 class Api::V1::WorldsController < ApplicationController
+
+  def index
+    worlds = World.all
+    comments = Comment.all
+    render json: worlds
+  end
+
+  def create
+    world = World.new(world_params)
+    if world.save
+      render json: world, status: :accepted
+    else
+      render json: {errors: world.errors.full_messages} status: :unprocessable_entity
+    end
+
+  end
+
+
+  private
+
+  def world_params
+    params.require(:world).permit(:name, :seed, :description, :image_url, :creator)
+  end
 end
