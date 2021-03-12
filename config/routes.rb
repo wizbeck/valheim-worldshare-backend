79a839ae810e294ad1a9d09efd3462caf58cecd0
Wesley <wes.beck21@outlook.com>
2021-03-11 20:11:09 -0800

    removed show route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 namespace :api do
   namespace :v1 do
-    resources :worlds, only: [:index, :create, :show, :update]
+    resources :worlds, only: [:index, :create, :update]
   end 
 end
 
