9e5548576a7207861e33630d37a3796d2ee38986
Wesley Beck <wes.beck21@outlook.com>
2022-06-11 14:56:07 -0600

    add jti for jwt token storage, schema updates for user relation to worlds and comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_04_140929) do
+ActiveRecord::Schema.define(version: 2022_06_11_202827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2022_06_04_140929) do
     t.string "author"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,7 +35,9 @@ ActiveRecord::Schema.define(version: 2022_06_04_140929) do
     t.string "steam_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "jti", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
@@ -46,6 +50,8 @@ ActiveRecord::Schema.define(version: 2022_06_04_140929) do
     t.string "creator"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_worlds_on_user_id"
   end
 
 end
