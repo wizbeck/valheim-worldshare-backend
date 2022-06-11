63e8d512b8f0b93486f5cdb657d9dcdaf5ccc59f
Wesley Beck <wes.beck21@outlook.com>
2022-06-11 14:58:04 -0600

    configure user devise macros for jwt authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :trackable
+         :trackable, :jwt_authenticatable, jwt_revocation_strategy: self
 
   has_many :worlds
   has_many :comments, through: :worlds
