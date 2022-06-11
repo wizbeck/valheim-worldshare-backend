11f4075897f46f5ad264db6bf39e2114676c2cba
Wesley Beck <wes.beck21@outlook.com>
2022-06-11 10:02:05 -0600

    [users, JWT] Stub out user controller actions and comments for JWT
    Add controller, with actions for handling new users and existing users
    Generate serializer for json formatting of user entities

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::API
+  # we need a before action with a private method checking authentication of a user when a request is made,
+  # and send an appropriate reponse back
 end
